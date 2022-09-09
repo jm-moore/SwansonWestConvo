d9bea7f9334834e2ec469664bea137e64571b961
jm-moore <jmmoore9358@gmail.com>
2022-09-09 13:54:05 -0500

    initial commit

---
+++ SwansonWestConvo/Methods.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwansonWestConvo
+{
+    public class Methods
+    {
+        public void SwansonQuote()
+        {
+            var client = new HttpClient();
+
+            var swansonURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            var swansonQuote = client.GetStringAsync(swansonURL).Result;
+
+            var swansonObject = JArray.Parse(swansonQuote);
+
+            Console.WriteLine($"Ron Swanson-\n{swansonObject[0]}");
+            Console.WriteLine();
+        }
+
+        public void WestQuote()
+        {
+            var client = new HttpClient();
+
+            var westUrl = "https://api.kanye.rest";
+
+            var westQuote = client.GetStringAsync(westUrl).Result;
+
+            var westObject = JObject.Parse(westQuote);
+
+            Console.WriteLine($"Kanye West-\n{westObject["quote"]}");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ SwansonWestConvo/Program.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json.Linq;
+using SwansonWestConvo;
+
+var convo = new Methods();
+
+for (int i = 0; i < 6; i++)
+{
+    convo.SwansonQuote();
+    Thread.Sleep(2500);
+    convo.WestQuote();
+    Thread.Sleep(2500);
+}
+
+
